f7ed3d7d76a9e7ca84cf559b4458712b42bfb628
David <dwrober@gmail.com>
2014-07-29 23:28:51 -0600

    create working now

--- app/controllers/pomodoros_controller.rb
+++ app/controllers/pomodoros_controller.rb
@@ -1,5 +1,5 @@
 class PomodorosController < ApplicationController
-
+skip_before_filter  :verify_authenticity_token
 
 def index
   @pomodoros = Pomodoro.all
@@ -9,5 +9,41 @@ def index
   end
 end
 
+def create
+  @pomodoro = Pomodoro.new
+  @pomodoro.user = "david"
+  respond_to do |format|
+    if @pomodoro.save
+      format.json { render :json => Pomodoro.first }
+    else
+      format.json { render json: @pomodoro.errors, status: :unprocessable_entity }
+    end
+  end
+  # respond_to do |format|
+  #   format.json { render :json => Pomodoro.first }
+  # end
+  # @pomodoro = Pomodoro.new
+  # @pomodoro.user = params[:pomodoro][:user]
+  # @pomodoro.task = params[:pomodoro][:task]
+  #   
+  # if @pomodoro.save
+  #   format.json { render json: @pomodoro, status: :created }
+  # else
+  #   format.json { render json: @pomodoro.errors, status: :unprocessable_entity }
+  # end
+  #format.json { render :json => @pomodoro }
+  # @user = current_user
+  # @workout = @user.workouts.build(params[:workout])
+  # @workouts =  @user.workouts.order("created_at desc")
+  # respond_to do |format|
+  #   if @workout.save
+  #     format.html { redirect_to workouts_path, notice: 'Workout successfully created.' }
+  #     format.json { render json: @workout, status: :created, location: workout_path(@workout) }
+  #   else
+  #     format.html { render action: "new" }
+  #     format.json { render json: @workout.errors, status: :unprocessable_entity }
+  #   end
+  # end
+end
 
 end
