a1f0398abe1ec3f8a98ee82aa7d1d41e2d361c92
David <dwrober@gmail.com>
2014-07-30 00:03:37 -0600

    basic display page

--- app/controllers/pomodoros_controller.rb
+++ app/controllers/pomodoros_controller.rb
@@ -5,7 +5,8 @@ def index
   @pomodoros = Pomodoro.all
   
   respond_to do |format|
-    format.json { render :json => @pomodoros }
+    format.html { render :html => @pomodoros }
+    format.json{ render :json => @pomodoros }
   end
 end
 
